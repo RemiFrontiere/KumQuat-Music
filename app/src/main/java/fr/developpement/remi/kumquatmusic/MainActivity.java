224359357c5f8da79c11748f1dfa2c1e4f8830bd
RemiFrontiere <remi.frontiere@gmail.com>
2017-07-28 17:44:26 +0200

    OnclickButtons

--- app/src/main/java/fr/developpement/remi/kumquatmusic/MainActivity.java
+++ app/src/main/java/fr/developpement/remi/kumquatmusic/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -16,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
 
     private Music maMusique;
     private ListView maListe;
+    private ImageButton precedent;
+    private ImageButton suivant;
+    private ImageButton pause;
+    private ImageButton stop;
 
 
     @Override
@@ -24,6 +29,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         this.maMusique = new Music();
         this.maListe = (ListView)findViewById(R.id.maListe);
+
+        this.precedent = (ImageButton)findViewById(R.id.btPrecedent) ;
+        this.suivant = (ImageButton)findViewById(R.id.btSuivant) ;
+        this.stop = (ImageButton)findViewById(R.id.btStop) ;
+        this.pause = (ImageButton)findViewById(R.id.btPause) ;
+        this.pause.setTag(R.mipmap.pause);
+
+
         ArrayList<LigneMusic> LignesMusics = genererLigneMusic();
         final MusicAdapter adapter = new MusicAdapter(MainActivity.this,LignesMusics);
         maListe.setAdapter(adapter);
@@ -31,7 +44,6 @@ public class MainActivity extends AppCompatActivity {
         maListe.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
 
                 if (maMusique.monPlayer != null && maMusique.monPlayer.isPlaying())
                 {
@@ -50,6 +62,47 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                    if ((Integer)pause.getTag() == R.mipmap.pause) {
+                        if (maMusique.monPlayer.isPlaying()) {
+                            maMusique.monPlayer.pause();
+                            pause.setImageResource(R.mipmap.play);
+                            pause.setTag(R.mipmap.play);
+                        }
+
+                    } else {
+                            maMusique.monPlayer.start();
+                            pause.setImageResource(R.mipmap.pause);
+                            pause.setTag(R.mipmap.pause);
+                    }
+                }
+
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(maMusique.monPlayer.isPlaying())
+                    maMusique.monPlayer.stop();
+            }
+        });
+
+        precedent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        suivant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
     }
     
     private ArrayList<LigneMusic> genererLigneMusic(){
@@ -60,6 +113,9 @@ public class MainActivity extends AppCompatActivity {
 
         return MesLignesMusics;
     }
+
+
+
     public void onDestroy() {
 
         if (maMusique.monPlayer != null && maMusique.monPlayer.isPlaying())
